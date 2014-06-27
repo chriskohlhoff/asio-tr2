352d0c75adc582bd695fbee6d531f54abd068ada
Christopher Kohlhoff <chris@kohlhoff.com>
2014-06-28 09:19:29 +1000

    Merge updated addstyle.pl from ip-address repository.

--- doc/addstyle.pl
+++ doc/addstyle.pl
@@ -17,17 +17,18 @@ my $style =
 "table { margin: auto; }" .
 "</style>";
 
+my $rev = `git rev-parse HEAD`;
+chomp($rev);
+my $draft_info = " (as of commit $rev)";
+$draft_info = "";
+
 my $docno =
 "<pre>" .
-"Doc. no:  N2175=07-0035\n" .
-"Date:     2007-03-11\n" .
-"Project:  Programming Language C++\n" .
-"Reply to: Christopher Kohlhoff &lt;chris\@kohlhoff.com&gt;\n" .
+"Doc. no:  D???? " . $draft_info . "\n" .
+"Date:     2014-??-??\n" .
+"Revises:  N2175\n" .
+"Reply-To: Christopher Kohlhoff &lt;chris\@kohlhoff.com&gt;\n" .
 "</pre>\n";
-
-my $copyright =
-"<hr/>\n" .
-"<em>Copyright &copy; 2006-2007 Christopher M. Kohlhoff</em>\n";
 
 my @lines = ();
 my $saw_h4 = 0;
@@ -38,7 +39,6 @@ while (my $line = <$file_in>)
 {
   $line =~ s~\<head\>~\<head\>$style~;
   $line =~ s~\<body bgcolor="white" text="black" link="#0000FF" vlink="#840084" alink="#0000FF"\>~\<body bgcolor="white" text="black" link="#0000FF" vlink="#840084" alink="#0000FF"\>$docno~;
-  $line =~ s~\<\/body\>~$copyright\<\/body\>~;
   $line =~ s/\240/ /g;
   if ($line =~ /<h4>/)
   {
